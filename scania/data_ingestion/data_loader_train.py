50399228bcce81f2c7afaeb50f534a4a7d6c0743
sethusaim <sethusaim@gmail.com>
2022-03-03 13:42:41 +0530

    made new changes

--- scania/data_ingestion/data_loader_train.py
+++ scania/data_ingestion/data_loader_train.py
@@ -1,38 +1,44 @@
-from scania.s3_bucket_operations.s3_operations import s3_operations
-from utils.logger import app_logger
+from scania.s3_bucket_operations.s3_operations import S3_Operation
+from utils.logger import App_Logger
 from utils.read_params import read_params
 
 
-class data_getter_train:
+class Data_Getter_Train:
     """
-    Description :   This class shall be used for obtaining the df from the source for training
+    Description :   This class shall be used for obtaining the df from the input files s3 bucket where the training file is present
+    Written by  :   iNeuron Intelligence
+    
     Version     :   1.2
-    Revisions   :   Moved to setup to cloud run setup
+    Revisions   :   Moved to setup to cloud 
     """
 
     def __init__(self, table_name):
         self.config = read_params()
 
-        self.training_file = self.config["export_csv_file"]["train"]
-
         self.table_name = table_name
 
-        self.input_files_bucket = self.config["s3_bucket"]["input_files_bucket"]
+        self.train_csv_file = self.config["export_csv_file"]["train"]
 
-        self.s3 = s3_operations()
+        self.input_files_bucket = self.config["bucket"]["input_files"]
 
-        self.log_writer = app_logger()
+        self.s3 = S3_Operation()
+
+        self.log_writer = App_Logger()
 
         self.class_name = self.__class__.__name__
 
     def get_data(self):
         """
         Method Name :   get_data
-        Description :   This method reads the data from the source
+        Description :   This method reads the data from the input files s3 bucket where the training file is stored
         Output      :   A pandas dataframe
-        On failure  :   Raise Exception
+        
+        On Failure  :   Write an exception log and then raise exception
+        Written by  :   iNeuron Intelligence
+        
         Version     :   1.2
-        Revisions   :   Moved to setup to cloud run setup
+        Written by  :   iNeuron Intelligence
+        Revisions   :   moved setup to cloud
         """
         method_name = self.get_data.__name__
 
@@ -45,8 +51,8 @@ class data_getter_train:
 
         try:
             df = self.s3.read_csv(
-                bucket=self.input_files_bucket,
-                file_name=self.input_files_bucket,
+                file_name=self.train_csv_file,
+                bucket_name=self.input_files_bucket,
                 table_name=self.table_name,
             )
 
